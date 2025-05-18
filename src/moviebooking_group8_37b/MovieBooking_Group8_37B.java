a17e8ffc8960f89b9f678ba68e149d67cfc63283
Kohinoor <kohinoorkapali@27gmail.com>
2025-05-18 16:41:10 +0545

    updated

---
+++ src/moviebooking_group8_37b/MovieBooking_Group8_37B.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package moviebooking_group8_37b;
+
+/**
+ *
+ * @author Kohinoor
+ */
+public class MovieBooking_Group8_37B {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
