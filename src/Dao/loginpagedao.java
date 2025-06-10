6dc74b9dae74664e785a5acb2600fcc759676328
Kohinoor <kohinoorkapali@27gmail.com>
2025-06-10 06:30:32 +0545

    added changes

--- src/Dao/loginpagedao.java
+++ src/Dao/loginpagedao.java
@@ -24,8 +24,8 @@ public class loginpagedao {
     String sql = "SELECT * FROM users WHERE username = ? AND password = ?";
 
     try (PreparedStatement stmt = conn.prepareStatement(sql)) {
-        stmt.setString(1, user.getUsername());
-        stmt.setString(2, user.getPassword());
+        stmt.setString(1, user.getUsername().trim());
+stmt.setString(2, user.getPassword().trim());
 
         ResultSet rs = stmt.executeQuery();
 
