8283c01603155be0f8837cd5898a310433dbbfc8
nitan <nitan@10.12.15.83>
2025-06-22 11:57:02 +0545

    connection modified.

--- src/Database/MySqlConnection.java
+++ src/Database/MySqlConnection.java
@@ -17,8 +17,8 @@ public class MySqlConnection implements Database{
     public Connection openConnection() {
        try{
            String username ="root";
-           String password="Nijisawa123%";
-           String database ="movie_practice";
+           String password="nitantghimire";
+           String database ="moviebookingproject";
            Connection connection;
            connection = DriverManager.getConnection(
            "jdbc:mysql://127.0.0.1:3306/"+database,username,password);
